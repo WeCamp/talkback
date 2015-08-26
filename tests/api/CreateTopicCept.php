8e09fa0f9f05491ed1bd0b9f06accc2b17b38d10
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-08-26 17:04:52 +0200

    Added tests for POST /topics response data

--- tests/api/CreateTopicCept.php
+++ tests/api/CreateTopicCept.php
@@ -2,6 +2,16 @@
 
 use Codeception\Module\ApiHelper;
 
+$testPostData     = [
+    'title'            => 'This is a cool topic!',
+    'details'          => 'Where is the seismic moon? Ships meet on mankind at atlantis tower! Transporter of a '.
+                          'sub-light energy, arrest the voyage! Why does the planet meet?  Planets die from '.
+                          'voyages like ship-wide space suits.',
+    'excerpt'          => 'Yuck! Pieces o\' life are forever swashbuckling. Madness ho! scrape to be commanded.',
+    'owned_by_creator' => true,
+];
+
+
 $I = new ApiTester($scenario);
 $I->wantTo('create a topic for me to talk about');
 
@@ -9,18 +19,16 @@ $I->wantTo('create a topic for me to talk about');
 $I->haveHttpHeader('Content-Type', 'application/json');
 
 // Submit
-$I->sendPOST(
-    ApiHelper::ENDPOINT_TOPICS,
-    [
-        'title'            => 'This is a cool topic!',
-        'details'          => 'Where is the seismic moon? Ships meet on mankind at atlantis tower! Transporter of a '.
-                              'sub-light energy, arrest the voyage! Why does the planet meet?  Planets die from '.
-                              'voyages like ship-wide space suits.',
-        'excerpt'          => 'Yuck! Pieces o\' life are forever swashbuckling. Madness ho! scrape to be commanded.',
-        'owned_by_creator' => true,
-    ]
-);
+$I->sendPOST(ApiHelper::ENDPOINT_TOPICS, $testPostData);
 
 // Response
 $I->seeResponseCodeIs(201);
 $I->seeResponseIsJson();
+$jsonResponse = $I->grabDataFromResponseByJsonPath('$');
+$I->canSeeResponseJsonMatchesJsonPath('$.id');
+$I->assertEquals($testPostData['details'], $I->grabDataFromResponseByJsonPath('$.details'));
+$I->assertEquals($testPostData['excerpt'], $I->grabDataFromResponseByJsonPath('$.excerpt'));
+$I->assertEquals($testPostData['owned_by_creator'], $I->grabDataFromResponseByJsonPath('$.owned_by_creator'));
+$I->canSeeResponseJsonMatchesJsonPath('$.created_at');
+
+// Db record
