8170d3e77ec8609871b4e5745da9d0e3a080ba97
Dennis Snijder <DennisSnijder97@Gmail.com>
2015-08-26 16:18:08 +0200

    added the ServiveControllerServiceProvider

--- web/index.php
+++ web/index.php
@@ -5,14 +5,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/', function() use($app) {
-    return 'Hello World';
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+$app['TopicController'] = $app->share(function() use ($app) {
+    return new Wecamp\TalkBack\Controller\TopicController($app);
 });
 
-$app->post('/topics', function() use($app) {
-    $topicController = new \Wecamp\TalkBack\Controller\TopicController();
-    return $topicController->newTopic();
-});
+$app->get('/', function() use($app) {
+    return 'Hello World';
+})->bind('homepage');
+
+$app->post('/topics', 'TopicController:newTopic');
 
 $app->get('/topics/{id}', function($id) use($app) {
     return $app->json($id);
