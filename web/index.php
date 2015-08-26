b500f439ce543c5f32fa6e069c27b0fa7d957746
Pascal de Vink <pascal.de.vink@gmail.com>
2015-08-26 16:30:20 +0200

    Added a /setup route to setup the database

--- web/index.php
+++ web/index.php
@@ -5,8 +5,36 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app['topicRepository'] = $app->share(function() use ($app) {
+    return new \Wecamp\TalkBack\Repository\TopicRepository();
+});
+
+$app['userRepository'] = $app->share(function() use ($app) {
+    return new \Wecamp\TalkBack\Repository\UserRepository();
+});
+
+$app['badgeRepository'] = $app->share(function() use ($app) {
+    return new \Wecamp\TalkBack\Repository\BadgeRepository();
+});
+
+$app['fixtures'] = $app->share(function() use ($app) {
+    return new \Wecamp\TalkBack\LoadFixtures(
+        $app['userRepository'],
+        $app['topicRepository'],
+        $app['badgeRepository']
+    );
+});
+
 $app->get('/', function() use($app) {
     return 'Hello World';
+});
+
+$app->get('/setup', function() use($app) {
+    /** @var \Wecamp\TalkBack\LoadFixtures $fixtures */
+    $fixtures = $app['fixtures'];
+    $fixtures->load();
+
+    return 'Setup complete!';
 });
 
 $app->post('/topics', function() use($app) {
