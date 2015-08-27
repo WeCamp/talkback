7701263045eb518739d720296910fc77258c3d27
Edwin Kortman <edwin@simplydread>
2015-08-27 11:30:34 +0200

    Adds setup up for show topic page

---
+++ web/assets/js/forms/showtopic.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+var ShowTopic = React.createClass({
+    render: function() {
+        return <div className="panel panel-default">
+            <div className="panel-heading clearfix">
+                <h3 className="panel-title pull-left">"var_dump() driven development"</h3>
+            </div>
+            <div className="panel-body">
+                
+            </div>
+        </div>
+    },
+});
+
+React.render(<ShowTopic/>, document.getElementById('showtopic'));
