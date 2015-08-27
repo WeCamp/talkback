98c734482e42e661919a2cd036865389201136cd
Dennis Snijder <DennisSnijder97@Gmail.com>
2015-08-27 11:27:33 +0200

    POST /api/topics creates a new topic
    Created Topic validation, TopicController, and made a function in the
    TopicRepository for adding Topics

--- web/index.php
+++ web/index.php
@@ -2,10 +2,20 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
 
 $app['topicRepository'] = $app->share(function() use ($app) {
     return new \Wecamp\TalkBack\Repository\TopicRepository();
@@ -28,7 +38,7 @@ $app['fixtures'] = $app->share(function() use ($app) {
 });
 
 $app['TopicController'] = $app->share(function() use ($app) {
-    return new Wecamp\TalkBack\Controller\TopicController($app);
+    return new \Wecamp\TalkBack\Controller\TopicController($app, $app['topicRepository']);
 });
 
 $app->get('/', function() use($app) {
@@ -62,10 +72,7 @@ $app->get('/setup', function() use($app) {
     return 'Setup complete!';
 });
 
-$app->post('/topics', 'TopicController:newTopic');
-
-$app->get('/topics/{id}', function($id) use($app) {
-    return $app->json($id);
-});
+$app->post('/api/topics', 'TopicController:newTopic');
+$app->get('/api/topics/{id}', 'TopicController:getTopic');
 
 $app->run();
