4425f94b0c2d032168bb94d6577f7e765e0e2005
Pascal de Vink <pascal.de.vink@gmail.com>
2015-08-27 15:07:04 +0200

    Added the list of topics

--- src/Wecamp/TalkBack/Repository/TopicRepository.php
+++ src/Wecamp/TalkBack/Repository/TopicRepository.php
@@ -72,7 +72,7 @@ final class TopicRepository extends BaseRepository
     public function getAllTopics()
     {
         $connection = $this->getConnection();
-        $insert =  "SELECT topic.title, topic.excerpt, topic.owned_by_creator, topic.created_at,
+        $insert =  "SELECT topic.id, topic.title, topic.excerpt, topic.owned_by_creator, topic.created_at,
             user.name as creator_name, count(vote.voter) as vote_count FROM topic LEFT JOIN vote on topic.id = vote.topic
             LEFT JOIN user on user.id = topic.creator GROUP BY topic.id";
         $stmt = $connection->prepare($insert);
